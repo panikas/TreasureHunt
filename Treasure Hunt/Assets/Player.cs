4f7335c27ad06e765bbffb2c9792a8ba35459809
Mattias Lindblad <panikas94@gmail.com>
2020-07-23 18:51:38 +0200

    initial commit

---
+++ Treasure Hunt/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+    public float speed = 5f, jumpHeight = 3f;
+    private Vector3 velocity;
+    private bool grounded = true;
+    void Start()
+    {
+            controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        
+          
+        controller.Move(velocity * Time.deltaTime);
+        if (GetComponent<CharacterController>().isGrounded) grounded = true;
+        if (Input.GetButton("Jump") && grounded)
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+            grounded = false;
+        }
+        if(grounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+        //if (move != Vector3.zero) transform.forward = move;
+    }
+
+}
+
---
+++ Treasure Hunt/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementForceFactor, speedMax, jumpForce;
+    private Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rBody.AddForce(this.transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(rBody.velocity.magnitude < speedMax) { 
+            rBody.AddForce(this.transform.forward * movementForceFactor);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (rBody.velocity.magnitude < speedMax/2)
+            {
+                rBody.AddForce(-this.transform.forward * movementForceFactor);
+            }
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(-this.transform.up * 2f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(this.transform.up * 2f);
+        }
+        
+    }
+}
